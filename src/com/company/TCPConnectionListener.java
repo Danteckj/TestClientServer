ca64145cd55a66e4e289a825e98b1ad1cb1139a9
danteckj <danteckj@gmail.com>
2017-11-19 23:10:59 +0300

    1.Разбил проект на модули и пакаджи. 1.В модуле клиента добавил поодержку для JavaFX.   1.1 Теперь интерфес можно рисовать просто через    конструктор а дальше редактировать через FXML.

--- src/com/company/TCPConnectionListener.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public interface TCPConnectionListener {
-
-    void onConectionReady(OurNet tcpConnection);
-    void onReceiveString(OurNet tcpConnection, String value);
-    void onDisconect(OurNet tcpConnection);
-    void onException(OurNet tcpConnection, Exception e);
-}
